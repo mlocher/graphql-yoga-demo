c5f0a93d606f5f8487577b09dce8b10f48fab86a
Marko Locher <marko@locher.at>
2023-05-12 15:06:25 +0200

    Add Stellate configuration

---
+++ stellate.ts
@@ -0,0 +1,53 @@
+import wtConfig from './src/config'
+import {
+    Config
+} from 'stellate'
+
+const config: Config = {
+    config: {
+        ignoreOriginCacheControl: true,
+        bypassCacheHeaders: [{
+            name: 'skip-cache'
+        }],
+        injectHeaders: true,
+        scopes: {
+            AUTHENTICATED_SELF_ONLY: "header:Authorization"
+        },
+
+        rootTypeNames: {
+            query: 'Query',
+            mutation: 'Mutation'
+        },
+        rules: [{
+                types: {
+                    Query: ['me']
+                },
+                maxAge: 900,
+                swr: 900,
+                scope: 'AUTHENTICATED_SELF_ONLY',
+                description: 'Cache current user and agency'
+            }
+        ],
+        retries: {
+            networkErrors: {
+                isEnabled: true,
+                whenGraphQLResponse: false
+            },
+            serverErrors: {
+                isEnabled: false
+            }
+        },
+        environments: {
+            "staging": {
+                "name": "mlocher-demo-yoga-staging",
+                "schema": "https://4igd49-4000.csb.app/graphql",
+                "originUrl": "https://4igd49-4000.csb.app/graphql",
+            }
+        },
+        "name": "mlocher-demo-yoga",
+        "schema": "https://4igd49-4000.csb.app/graphql",
+        "originUrl": "https://4igd49-4000.csb.app/graphql",
+    }
+}
+
+export default config
