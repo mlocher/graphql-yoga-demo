b193a6aebad9b4cacc7a4b90109e993116dc14e6
Marko Locher <marko@locher.at>
2023-06-01 11:54:43 +0200

    Add support for defer/stream and custom CORS

--- schema.js
+++ schema.js
@@ -1,10 +1,16 @@
 import { createSchema } from "graphql-yoga";
 
+const wait = (time) =>
+  new Promise((resolve) => setTimeout(resolve, time))
+
 export const schema = createSchema({
   typeDefs: /* GraphQL */ `
     scalar File
     type Query {
       hello(name: String): String
+      alphabet: [String!]!
+      fastField: String!
+      slowField(waitFor: Int! = 5000): String
     }
     type Mutation {
       echo(message: String!): String
@@ -17,6 +23,20 @@ export const schema = createSchema({
         console.log(context.request.headers);
         return _args["name"] || "world";
       },
+      async *alphabet() {
+        for (const character of ['a', 'b', 'c', 'd', 'e', 'f', 'g']) {
+          yield character
+          await wait(1000)
+        }
+      },
+      fastField: async () => {
+        await wait(100)
+        return 'I am speedy'
+      },
+      slowField: async (_, { waitFor }) => {
+        await wait(waitFor)
+        return 'I am slow'
+      }
     },
     Mutation: {
       echo: (_, _args, context) => {
--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 import { createServer } from "node:http";
 import { createYoga } from "graphql-yoga";
+import { useDeferStream } from '@graphql-yoga/plugin-defer-stream'
 import { schema } from "./schema.js";
 
 // Create a Yoga instance with a GraphQL schema.
-const yoga = createYoga({ schema });
+const yoga = createYoga({
+  schema,
+  cors: {
+    origin: 'http://localhost:4000',
+    credentials: true,
+    allowedHeaders: ['X-Custom-Header'],
+    methods: ['POST']
+  },
+  plugins: [useDeferStream()]
+});
 
 // Pass it into a server to hook into request handlers.
 const server = createServer(yoga);
