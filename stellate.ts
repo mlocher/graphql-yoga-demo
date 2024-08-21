33341a6941ac387fddeee4dc938c77a344ceb5a5
Marko Locher <marko@locher.at>
2024-08-21 14:19:30 +0000

    Update Stellate configuration

--- stellate.ts
+++ stellate.ts
@@ -1,61 +1,35 @@
-import wtConfig from './src/config'
-import {
-    Config
-} from 'stellate'
+import { Config } from "stellate";
 
 const config: Config = {
-    config: {
-        ignoreOriginCacheControl: true,
-        bypassCacheHeaders: [{
-            name: 'skip-cache'
-        }],
-        injectHeaders: true,
-        scopes: {
-            AUTHENTICATED_SELF_ONLY: "header:Authorization",
-            AUTHENTICATED_JWT: {
-                definition: "header:Authorization",
-                jwt: {
-                    claim: 'sub',
-                    algorithm: 'HS256',
-                    secret: 'secret'
-                }
-            }
-        },
-
-        rootTypeNames: {
-            query: 'Query',
-            mutation: 'Mutation'
-        },
-        rules: [{
-                types: {
-                    Query: ['me']
-                },
-                maxAge: 900,
-                swr: 900,
-                scope: 'AUTHENTICATED_JWT',
-                description: 'Cache current user and agency'
-            }
-        ],
-        retries: {
-            networkErrors: {
-                isEnabled: true,
-                whenGraphQLResponse: false
-            },
-            serverErrors: {
-                isEnabled: false
-            }
-        },
-        environments: {
-            "staging": {
-                "name": "mlocher-demo-yoga-staging",
-                "schema": "https://4igd49-4000.csb.app/graphql",
-                "originUrl": "https://4igd49-4000.csb.app/graphql",
-            }
-        },
-        "name": "mlocher-demo-yoga",
-        "schema": "https://4igd49-4000.csb.app/graphql",
-        "originUrl": "https://4igd49-4000.csb.app/graphql",
-    }
-}
-
-export default config
+  config: {
+    name: "mlocher-demo-yoga",
+    originUrl: "https://f7gxsr-5000.csb.app/graphql",
+    schema: "https://f7gxsr-5000.csb.app/graphql",
+    schemaPolling: {
+      enabled: true,
+    },
+    cacheIntrospection: true,
+    mutationPolicy: "List",
+    rootTypeNames: {
+      query: "Query",
+      mutation: "Mutation",
+    },
+    rules: [
+      {
+        types: ["Query"],
+        maxAge: 86400,
+        swr: 600,
+      },
+      {
+        types: ["Category"],
+        maxAge: 5,
+        swr: 0,
+      },
+    ],
+    nonCacheable: [],
+    requestSigning: {
+      secret: "my-super-secret-secret",
+    },
+  },
+};
+export default config;
